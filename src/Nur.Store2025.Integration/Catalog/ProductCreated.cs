d3dd8682f93efa81283250c6ddfd86e540dcfab6
Jose Carlos Gutierrez <joseco48@gmail.com>
2025-05-03 06:37:30 -0400

    Mensajes de Integracion de la tienda

---
+++ src/Nur.Store2025.Integration/Catalog/ProductCreated.cs
@@ -0,0 +1,18 @@
+﻿using Joseco.Communication.External.Contracts.Message;
+
+namespace Nur.Store2025.Integration.Catalog;
+
+public record ProductCreated : IntegrationMessage
+{
+    public Guid ProductId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public ProductCreated(Guid productId, string name, string description, string? correlationId = null, string? source = null)
+        :base(correlationId, source)
+    {
+        ProductId = productId;
+        Name = name;
+        Description = description;
+    }
+}
---
+++ src/Nur.Store2025.Integration/Identity/UserCreated.cs
@@ -0,0 +1,19 @@
+﻿using Joseco.Communication.External.Contracts.Message;
+
+namespace Nur.Store2025.Integration.Identity;
+
+public record UserCreated : IntegrationMessage
+{
+    public Guid UserId { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+
+    public UserCreated(Guid userId, string fullName, string email, string? correlationId = null, string? source = null)
+        : base(correlationId, source)
+    {
+        UserId = userId;
+        FullName = fullName;
+        Email = email;
+    }
+
+}
