f952d807761aaea73b06403a7b6f1376896bc05a
Jose Carlos Gutierrez <joseco48@gmail.com>
2025-06-02 11:44:16 -0400

    Se agregaron los mensajes de integracion para la Saga relacionada con la creacion de ordenes

---
+++ src/Nur.Store2025.Integration/Inventory/InventoryReservationFailed.cs
@@ -0,0 +1,14 @@
+﻿using Joseco.Communication.External.Contracts.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nur.Store2025.Integration.Inventory;
+
+public record InventoryReservationFailed : IntegrationMessage
+{
+    public Guid OrderId { get; set; }
+    public string Reason { get; set; }
+}
---
+++ src/Nur.Store2025.Integration/Inventory/InventoryReserved.cs
@@ -0,0 +1,14 @@
+﻿using Joseco.Communication.External.Contracts.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nur.Store2025.Integration.Inventory;
+
+public record InventoryReserved : IntegrationMessage
+{
+    public Guid SourceId { get; set; }
+    public string SourceType { get; set; }
+}
